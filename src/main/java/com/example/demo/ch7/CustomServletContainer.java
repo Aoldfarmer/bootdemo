0e9cd53e343b0c8d7747b9edb5371a9955a12816
Aoldfarmer <738648406@qq.com>
2017-11-20 17:18:47 +0800

    update

---
+++ src/main/java/com/example/demo/ch7/CustomServletContainer.java
@@ -0,0 +1,18 @@
+package com.example.demo.ch7;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class CustomServletContainer implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8888);
+        container.setSessionTimeout(10, TimeUnit.MINUTES);
+    }
+}
